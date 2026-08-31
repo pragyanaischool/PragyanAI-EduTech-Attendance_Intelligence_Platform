fa2281f4c4ec5cf47ee0cc540bf0858f33363111
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-08-31 23:19:42 +0530

    Initialize views package with user-wise dashboards
    Added imports for various user-wise views and dashboards.

---
+++ views/__init__.py
@@ -0,0 +1,11 @@
+# Modular User-Wise Views & Dashboards Package
+from .student_view import render_student_dashboard
+from .faculty_view import render_faculty_dashboard
+from .parent_view import render_parent_dashboard
+from .hod_view import render_hod_dashboard
+from .principal_view import render_principal_dashboard
+from .admin_view import render_admin_dashboard
+from .ai_chat import render_ai_chat_view
+from .leaves import render_leave_portal
+from .reports import render_reports_view
+from .profile import render_profile_view
